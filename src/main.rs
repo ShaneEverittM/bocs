35059f1089197b244d9b65a1548036438464c912
ShaneEverittM <shane.everitt.murphy@gmail.com>
2021-02-25 01:51:38 -0800

    Main, root error.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use thiserror::Error;
+
 mod hasher;
 mod parser;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Error, Debug)]
+enum EPPError {
+    #[error(transparent)]
+    ParserError(#[from] parser::ParserError),
+}
+
+fn main() -> Result<(), EPPError> {
+    let mut motifs = Vec::new();
+    while let Some(mi) = parser::parse_motif(&mut std::io::stdin().lock())? {
+        motifs.push(mi);
+    }
+    dbg!(&motifs);
+    Ok(())
 }
