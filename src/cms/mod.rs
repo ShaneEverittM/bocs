f2127a1d9b37f48563652c7dab37a24c7dabf7bc
Shane Murphy <shaneeverittmurphy@gmail.com>
2021-03-08 20:19:52 -0800

    Calculate best prime to give Hasher.

--- src/cms/mod.rs
+++ src/cms/mod.rs
@@ -3,13 +3,27 @@
 
 pub mod hash;
 
+use hash::Hasher;
+
 type Count = u16;
+
+static PRIMES: &[usize] = &[
+    32729,
+    271927,
+    2718409,
+    33554467,
+    271828199,
+    4294967311,
+    34359738421,
+    271828182863,
+];
 
 /// A specialized implementation of the
 /// [Count-Min Sketch data structure](https://en.wikipedia.org/wiki/Count%E2%80%93min_sketch).
 pub struct CountMinSketch {
     depth: usize,
     width: usize,
+    hasher: Hasher,
     table: Vec<Vec<Count>>,
 }
 
@@ -20,11 +34,19 @@ impl CountMinSketch {
     pub fn new(error_rate: f64, confidence: f64) -> Self {
         let depth = f64::ceil(f64::ln(1.0 / (1.0 - confidence / 100.0))) as usize;
         let width = f64::ceil(std::f64::consts::E / error_rate) as usize;
-        dbg!(depth);
-        dbg!(width);
+
+        let mut prime = 0;
+        for &potential_prime in PRIMES.iter() {
+            if width < potential_prime {
+                prime = potential_prime;
+                break;
+            }
+        }
+
         Self {
             depth,
             width,
+            hasher: Hasher::with_prime(prime),
             table: vec![vec![0; width]; depth],
         }
     }
@@ -58,13 +80,13 @@ impl CountMinSketch {
 
     fn cms_hash(&self, raw: &str, idx: usize) -> usize {
         match idx {
-            0 => hash::js_hash(raw),
-            1 => hash::bkdr_hash(raw),
-            2 => hash::rs_hash(raw),
-            3 => hash::string_fold_hash(raw),
-            4 => hash::pjw_hash(raw),
-            5 => hash::elf_hash(raw),
-            6 => hash::sdbm_hash(raw),
+            0 => self.hasher.js_hash(raw),
+            1 => self.hasher.bkdr_hash(raw),
+            2 => self.hasher.rs_hash(raw),
+            3 => self.hasher.string_fold_hash(raw),
+            4 => self.hasher.pjw_hash(raw),
+            5 => self.hasher.elf_hash(raw),
+            6 => self.hasher.sdbm_hash(raw),
             _ => unimplemented!(),
         }
     }
