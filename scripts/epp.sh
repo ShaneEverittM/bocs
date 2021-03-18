bea41793d922f9a22c47dc8cac44abfc9490f455
Shane Murphy <shaneeverittmurphy@gmail.com>
2021-03-17 23:31:30 -0700

    Update script.

--- scripts/epp.sh
+++ scripts/epp.sh
@@ -5,24 +5,25 @@
 # Incoming parameters
 k=
 n=
-e=
 
-# Expected files
+# Assumed values
 BLANT="/home/wayne/pub/cs295p/blant-mp.sh"
 INPUT="/extra/wayne1/preserve/cs295p/EdgePrediction/HI-union.el"
 TIME="/usr/bin/time"
+E=7
+
 
 usage() {
-  echo "Usage: epp.sh -k <k> -n <n> -e <e> [-h]"
+  echo "Usage: epp.sh -k <k> -n <n> [-h]"
 }
 
-while getopts ":h?k:n:e:" opt; do
+while getopts ":h?k:n:" opt; do
   case ${opt} in
   h | \?)
     echo "A wrapper around the epp executable meant to facilitate running "
     echo "it in its intended manner: as an extension TO BLANT. This script will "
     echo "pass the parameters k and n to BLANT as expected, but also pass k "
-    echo "and e to epp, as well as create a folder for the output named based "
+    echo "to epp, as well as create a folder for the output named based "
     echo "on the parameters."
     echo ""
     usage
@@ -38,9 +39,6 @@ while getopts ":h?k:n:e:" opt; do
   n)
     n=$OPTARG
     ;;
-  e)
-    e=$OPTARG
-    ;;
   esac
 done
 
@@ -50,15 +48,15 @@ shift $((OPTIND - 1))
 # declare remaining args to not be handled by getopts
 [ "${1:-}" = "--" ] && shift
 
-if [ -v "$k" ] | [ -v "$n" ] | [ -v "$e" ]; then
+if [ -v "$k" ] | [ -v "$n" ] | [ -v "$E" ]; then
   usage
   exit 1
 fi
 
-OUT="./k$k-n$n-e$e-output"
+OUT="./k$k-n$n-e$E-output"
 
 if [ -d "$OUT" ]; then
-  echo "Directory $OUT already exists."
+  echo "Directory $OUT already exists." 1>&2
   exit 1
 else
   mkdir "$OUT"
@@ -68,7 +66,12 @@ echo "Outputting to $OUT"
 
 echo "Running..."
 
+LINES=$(wc -l $INPUT | cut -f1 -d' ')
+range=$(bc -l <<< "1.0 /(10^$E) * $LINES")
+
+echo "Covered $LINES of input with k=$k, e=$E and a range of $range" > "$OUT"/epp_stats.txt
+
 $TIME -v -o "$OUT"/total_time.txt sh -c "$TIME -v -o $OUT/blant_time.txt $BLANT -k$k -n$n $INPUT \
-	| $TIME -v -o $OUT/epp_time.txt epp -k$k -e$e > $OUT/epp_output.txt"
+	| $TIME -v -o $OUT/epp_time.txt epp -k$k -e$E > $OUT/epp_output.txt"
 
 echo "Done!"
